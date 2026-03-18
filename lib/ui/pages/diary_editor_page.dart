c6f977654353f45d1b8cb0d37d159dccbfab5771
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2026-03-17 22:02:57 -0300

    Revert "fix(editor): handle mobile keyboard overlap"
    This reverts commit dc9461f259a63a96c4e55f0316c2457b73f2078e.

--- lib/ui/pages/diary_editor_page.dart
+++ lib/ui/pages/diary_editor_page.dart
@@ -35,7 +35,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
   late final QuillController _contentController;
   final FocusNode _editorFocusNode = FocusNode();
   final ScrollController _editorScrollController = ScrollController();
-  final GlobalKey _editorContainerKey = GlobalKey();
 
   bool _isPublic = false;
   bool _isUpdatingAccess = false;
@@ -49,7 +48,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
       document: Document(),
       selection: const TextSelection.collapsed(offset: 0),
     );
-    _editorFocusNode.addListener(_handleEditorFocus);
     _isPublic = widget.diary.isPublic;
     _selectedDate = _normalizeDate(widget.initialDate ?? DateTime.now());
     _loadEntryForDate(_selectedDate);
@@ -57,7 +55,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
 
   @override
   void dispose() {
-    _editorFocusNode.removeListener(_handleEditorFocus);
     _contentController.dispose();
     _editorScrollController.dispose();
     _editorFocusNode.dispose();
@@ -334,35 +331,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
     return document;
   }
 
-  void _handleEditorFocus() {
-    if (!_editorFocusNode.hasFocus || !mounted) {
-      return;
-    }
-
-    final isCompact = MediaQuery.sizeOf(context).width < 700;
-    if (!isCompact) {
-      return;
-    }
-
-    final targetContext = _editorContainerKey.currentContext;
-    if (targetContext == null) {
-      return;
-    }
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) {
-        return;
-      }
-
-      Scrollable.ensureVisible(
-        targetContext,
-        duration: const Duration(milliseconds: 250),
-        curve: Curves.easeOut,
-        alignment: 0.2,
-      );
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final isCompact = MediaQuery.sizeOf(context).width < 700;
@@ -378,7 +346,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
             contentController: _contentController,
             editorFocusNode: _editorFocusNode,
             editorScrollController: _editorScrollController,
-            editorContainerKey: _editorContainerKey,
             onSelectDate: _selectDate,
             onChangeDay: _changeDay,
             onChangeMonth: _changeMonth,
@@ -393,7 +360,6 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
             contentController: _contentController,
             editorFocusNode: _editorFocusNode,
             editorScrollController: _editorScrollController,
-            editorContainerKey: _editorContainerKey,
             onSelectDate: _selectDate,
             onChangeDay: _changeDay,
             onChangeMonth: _changeMonth,
@@ -401,23 +367,16 @@ class _DiaryEditorPageState extends State<DiaryEditorPage> {
             onSave: _saveContent,
           );
 
-    final keyboardInset = MediaQuery.viewInsetsOf(context).bottom;
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.diary.name),
       ),
-      body: AnimatedPadding(
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.easeOut,
-        padding: EdgeInsets.only(bottom: isCompact ? keyboardInset : 0),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 1100),
-            child: AppSurfaceCard(child: content),
-          ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 1100),
+          child: AppSurfaceCard(child: content),
         ),
       ),
     );
